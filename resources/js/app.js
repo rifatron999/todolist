846dc0214d2b5e90a99c6e1031f87266194f4685
rifatron999 <rifatron999@gmail.com>
2022-09-14 00:39:53 +0600

    front end done

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,15 @@ require('./bootstrap');
 
 import Vue from 'vue'
 import App from './vue/app'
+//font awesome for vue 2
+    import { library } from '@fortawesome/fontawesome-svg-core'
+    import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+    library.add(faPlusSquare, faTrash)
+    Vue.component('font-awesome-icon', FontAwesomeIcon)
+//font awesome for vue 2 #
+
+
 
 const app = new Vue({
     el: '#app',
--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -2,9 +2,14 @@
     <div class="todoListContainer">
        <div class="heading">
             <h2 id="title">Todo List</h2>
-            <add-item-form/>
+            <add-item-form
+                v-on:reloadlist="getList()"
+            />
        </div>
-       <list-view/>
+       <list-view 
+            :items="items"
+            v-on:reloadlist="getList()"
+       />
     </div>
 </template>
 <script>
@@ -14,7 +19,27 @@
         components: {
             addItemForm,
             listView
+        },
+        data: function(){
+            return {
+                items: []
+            }
+        },
+        methods: {
+            getList(){
+                axios.get('api/items')
+                .then(response =>{
+                    this.items = response.data
+                })
+                .catch( error => {
+                    console.log( error);
+                })
+            }
+        },
+        created(){
+            this.getList();
         }
+
     }
 </script>
 <style scoped>
